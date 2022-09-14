0107fdcdf60353172503e0f9a9aad222eeb3f368
Yarin Matmoni <yarinmatmoni@gmail.com>
2022-09-14 19:03:01 +0300

    add utils for content add header component fix navbar - style and functionality

---
+++ components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Header.module.scss';
+import Navbar from '../navbar/Navbar';
+
+function Header() {
+    return (
+        <header className={styles.header}>
+            <Navbar />
+        </header>
+    )
+};
+
+export default Header;
