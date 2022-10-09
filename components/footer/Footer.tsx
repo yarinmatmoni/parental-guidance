fd13e2738aa03df1f035aade62b6070235db6041
tomerbarak <tomer.barak@dynamicyield.com>
2022-10-09 12:19:14 +0300

    refactor

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,64 +1,87 @@
-import React from 'react';
-import styles from './Footer.module.scss';
-import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../../public/logo/logo1.svg';
-import * as Content from '../../utils/content/common';
+import React from "react";
+import styles from "./Footer.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/logo/logo1.svg";
+import * as Content from "../../utils/content/common";
+import Logo from "../logo/Logo";
 
 function Footer() {
-    return (
-        <footer className={styles.footerContainer}>
-            <div className={styles.content}>
-                <div className={styles.descriptionLogo}>
-                    <div className={styles.logo}>
-                        <Image src={logo} height={80} width={100} alt='logo'></Image>
-                    </div>
-                    <div className={styles.description}>
-                        <h4>כמה מילים</h4>
-                    </div>
-                </div>
-                <div className={styles.navigation}>
-                    <div className={styles.navContainer}>
-                        <ul>
-                            <h4>כללי</h4>
-                            {Content.nav.filter((menuItem) => menuItem.sub.length === 0).map((item, i) => (
-                                <Link key={i} href="/"><a>{item.name}</a></Link>
-                            ))}
-                        </ul>
-                        {Content.nav.filter((menuItem) => menuItem.sub.length !== 0).map((item, i) => (
-                            <ul key={i}>
-                                <h4>{item.name}</h4>
-                                {item.sub.map((subMenuItem, i) => <Link key={i} href="/"><a>{subMenuItem.name}</a></Link>)}
-                            </ul>
-                        ))}
-                    </div>
-                </div>
-                <div className={styles.contact}>
-                    <h4>יצירת קשר</h4>
-                    <div className={styles.contactDetails}>
-                        <div className={styles.top}>
-                            <div className={styles.topItem}>
-                                <span className="material-symbols-outlined">call</span>
-                                <h4>{Content.personalInfo.phoneNumber.display}</h4>
-                            </div>
-                            <div className={styles.topItem}>
-                                <span className="material-symbols-outlined">mail</span>
-                                <h4>shalhevetbarak@gmail.com</h4>
-                            </div>
-                        </div>
-                        <div className={styles.bottom}>
-                            <a href={`//api.whatsapp.com/send?phone=972${Content.personalInfo.phoneNumber.value}&text=${Content.whatsAppMessage}`} target="_blank" rel="noreferrer">
-                                <Image src='/icons/whatsapp_icon.svg' height={30} width={30} alt="whatsapp_icon" ></Image>
-                            </a>
-                            <a>
-                                <Image src='/icons/facebook_icon.svg' height={30} width={30} alt="facebook_icon" ></Image>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className={styles.footerContainer}>
+      <div className={styles.content}>
+        <div className={styles.descriptionLogo}>
+          <Logo height={80} width={100} />
+          <div className={styles.description}>
+            <h4>כמה מילים</h4>
+          </div>
+        </div>
+        <div className={styles.navigation}>
+          <div className={styles.navContainer}>
+            <ul>
+              <h4>כללי</h4>
+              {Content.nav
+                .filter((menuItem) => menuItem.sub.length === 0)
+                .map((item, i) => (
+                  <Link key={i} href="/">
+                    <a>{item.name}</a>
+                  </Link>
+                ))}
+            </ul>
+            {Content.nav
+              .filter((menuItem) => menuItem.sub.length !== 0)
+              .map((item, i) => (
+                <ul key={i}>
+                  <h4>{item.name}</h4>
+                  {item.sub.map((subMenuItem, i) => (
+                    <Link key={i} href="/">
+                      <a>{subMenuItem.name}</a>
+                    </Link>
+                  ))}
+                </ul>
+              ))}
+          </div>
+        </div>
+        <div className={styles.contact}>
+          <h4>יצירת קשר</h4>
+          <div className={styles.contactDetails}>
+            <div className={styles.top}>
+              <div className={styles.topItem}>
+                <span className="material-symbols-outlined">call</span>
+                <h4>{Content.personalInfo.phoneNumber.display}</h4>
+              </div>
+              <div className={styles.topItem}>
+                <span className="material-symbols-outlined">mail</span>
+                <h4>shalhevetbarak@gmail.com</h4>
+              </div>
             </div>
-        </footer >
-    )
-};
+            <div className={styles.bottom}>
+              <a
+                href={`//api.whatsapp.com/send?phone=972${Content.personalInfo.phoneNumber.value}&text=${Content.whatsAppMessage}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  src="/icons/whatsapp_icon.svg"
+                  height={30}
+                  width={30}
+                  alt="whatsapp_icon"
+                ></Image>
+              </a>
+              <a>
+                <Image
+                  src="/icons/facebook_icon.svg"
+                  height={30}
+                  width={30}
+                  alt="facebook_icon"
+                ></Image>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,63 +1,70 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../../public/logo/logo1.svg';
-import * as Content from '../../utils/content/common';
-import styles from './Navbar.module.scss';
+import React, { useRef, useState, useEffect } from "react";
+import Link from "next/link";
+import * as Content from "../../utils/content/common";
+import styles from "./Navbar.module.scss";
+import Logo from "../logo/Logo";
+import Hamburger from "../navbar/hamburger/Hamburger";
 
 function Navbar() {
-    const refMenuContainer = useRef<HTMLUListElement>(null);
-    // const refIconBurger = useRef<HTMLDivElement>(null);
-    const [isOpen, setIsOpen] = useState(false);
+  const refMenuContainer = useRef<HTMLUListElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-        const handleClickOutside = (event: any) => {
-            if (refMenuContainer.current && !refMenuContainer.current.contains(event.target)) {
-                setIsOpen(false);
-            }
-        }
-
-        document.addEventListener('click', handleClickOutside, true);
-
-        return () => {
-            document.removeEventListener('click', handleClickOutside, true);
-        }
-
-    }, []);
-
-    const onMenuClick = () => {
-        setIsOpen(!isOpen);
-
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (
+        refMenuContainer.current &&
+        !refMenuContainer.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
     };
 
-    return (
-        <nav ref={refMenuContainer} className={styles.nav}>
-            <div className={styles.logo}>
-                <Image src={logo} height={75} width={75} alt="logo"></Image>
-            </div>
-            <ul className={styles.navMenu} data-open={isOpen}>
-                <div className={styles.wrapper}>
-                    {Content.nav && Content.nav.map((menuItem, i) => (
-                        <li key={i}> {menuItem.sub.length > 0 ? menuItem.name : <Link href={`${menuItem.link}`}><a onClick={onMenuClick}>{menuItem.name}</a></Link>}
-                            {menuItem.sub.length > 0 ?
-                                <ul className={styles.subMenu} data-open={isOpen}>
-                                    {menuItem.sub.map((subMenuItem, i) => (
-                                        <Link key={i} className={styles.link} href={`${subMenuItem.link}`}><a onClick={onMenuClick}>{subMenuItem.name}</a></Link>
-                                    ))}
-                                </ul>
-                                : ''}
-                        </li>
+    document.addEventListener("click", handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  const onMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav ref={refMenuContainer} className={styles.navMenuContainer}>
+      <Logo height={75} width={75} />
+      <ul className={styles.menuContainer} data-open={isOpen}>
+        <>
+          {Content.nav?.map((menuItem, i) => (
+            <li key={i} className={styles.menuItem}>
+              {menuItem.sub.length > 0 ? (
+                <>
+                  {menuItem.name}
+                  <ul className={styles.subMenu} data-open={isOpen}>
+                    {menuItem.sub.map((subMenuItem, i) => (
+                      <li key={i} className={styles.menuItem}>
+                        <Link
+                          className={styles.link}
+                          href={`${subMenuItem.link}`}
+                        >
+                          <a onClick={onMenuClick}>{subMenuItem.name}</a>
+                        </Link>
+                      </li>
                     ))}
-                </div>
-            </ul>
-            <div onClick={onMenuClick} className={styles.hamburger} data-open={isOpen}>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
-                <span className={styles.bar}></span>
-            </div>
-        </nav>
-    )
-};
+                  </ul>
+                </>
+              ) : (
+                <Link href={`${menuItem.link}`}>
+                  <a onClick={onMenuClick}>{menuItem.name}</a>
+                </Link>
+              )}
+            </li>
+          ))}
+        </>
+      </ul>
+      <Hamburger onMenuClick={onMenuClick} isOpen={isOpen} />
+    </nav>
+  );
+}
 
 export default Navbar;
