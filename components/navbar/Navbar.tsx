d9a35085f6283a353ef9dffe11f11b3db7d70194
Yarin Matmoni <yarinmatmoni@gmail.com>
2022-10-10 04:48:29 +0300

    display the active page - navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from 'next/router';
 import * as Content from "../../utils/content/common";
 import styles from "./Navbar.module.scss";
 import Logo from "../logo/Logo";
@@ -8,6 +9,7 @@ import Hamburger from "../hamburger/Hamburger";
 function Navbar() {
   const refMenuContainer = useRef<HTMLUListElement>(null);
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     const handleClickOutside = (event: any) => {
@@ -36,7 +38,7 @@ function Navbar() {
       <ul className={styles.menuContainer} data-open={isOpen}>
         <>
           {Content.nav?.map((menuItem, i) => (
-            <li key={i} className={styles.menuItem}>
+            <li key={i} className={styles.menuItem} data-path={router.pathname === menuItem.link ? true : false}>
               {menuItem.sub.length > 0 ? (
                 <>
                   {menuItem.name}
