fe7bf09dc3557bdb0b11a0e010302773c02957f3
Yarin Matmoni <yarinmatmoni@gmail.com>
2022-11-02 20:59:17 +0200

    importance section

--- pages/Advice&Guidance/Schedule.tsx
+++ pages/Advice&Guidance/Schedule.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import * as Data from '../../utils/content/schedulePage';
 import { motion } from "framer-motion";
 import * as Animation from "../../utils/animation/animation";
+import Shape from "../../components/shape/Shape";
 import styles from '../../styles/Schedule.module.scss';
 
 function Schedule() {
@@ -16,10 +17,18 @@ function Schedule() {
                 <motion.h1 variants={Animation.titleAnimation}>סדר יום לילדים</motion.h1>
                 <p>{Data.intro.introParagraph}</p>
                 <h3>{Data.intro.title}</h3>
-                <p>{Data.intro.endParagraph}</p>
+                <p>{Data.intro.endParagraph.line1}</p>
+                <p>{Data.intro.endParagraph.line2}</p>
+                <Shape name="shape1" height={300} top={-50} left={220} opacity={0.35} rotate={210} />
             </section>
             <section className={`section ${styles.importance}`}>
-
+                <h3>{Data.importance.title}</h3>
+                <ul>
+                    {Data.importance.list.map((item, index) => (
+                        <li key={`${index}_${item}`}>{item}</li>
+                    ))}
+                </ul>
+                <Shape name="shape3" height={250} top={250} left={20} opacity={0.15} rotate={0} />
             </section>
         </motion.div>
     )
